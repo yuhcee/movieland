a4c5100e9e1a4496307fce3f024a10e3b065b65e
yuhcee <egbouchenna001@gmail.com>
2022-03-17 01:36:52 +0100

    movies: fix errors

--- src/pages/movies.jsx
+++ src/pages/movies.jsx
@@ -1,34 +1,36 @@
 import { useEffect, useState } from 'react';
 import SearchIcon from '../assets/search.svg';
 import MovieCard from '../components/MovieCard';
+import debounce from '../utils/debounce';
 
 const Movies = () => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
     const API_URL = 'http://www.omdbapi.com/?apikey=d0916aed&';
 
-    const searchMovies = async (title) => {
+    const searchMovies = async (title = 'movie') => {
+        if (title === '') return;
         const response = await fetch(`${API_URL}s=${title}`);
         const data = await response.json();
         setMovies(data.Search);
     };
 
-    const handleChange = (e) => setSearchTerm(e.target.value);
-    const handleClick = (title) => searchMovies(title);
+    const handleChange = debounce((e) => {
+        setSearchTerm(e.target.value);
+    }, 1000);
 
     useEffect(() => {
-        searchMovies('spider');
-    }, []);
+        searchMovies();
+    }, [searchTerm]);
 
     return (
         <>
             <h1> Movie Mania </h1>
             <div className="search">
-                <input placeholder="Search for movies" value={searchTerm} onChange={handleChange} />
-                <img src={SearchIcon} alt="Search" onClick={() => handleClick(searchTerm)} />
+                <input placeholder="Search for movies" onChange={handleChange} />
+                <img src={SearchIcon} alt="Search" onClick={() => searchMovies(searchTerm)} />
             </div>
-
-            {movies.length > 0 ? (
+            {movies !== undefined ? (
                 <div className="container">
                     {movies.map((movie, idx) => (
                         <MovieCard key={idx} movie={movie} />
